5370b7a321107873a41862a9988f1c523f4f15dc
Michael Milton <ttmigueltt@gmail.com>
2017-03-08 14:24:44 +1100

    Fixed input parameter format, explicitly use perl

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
-# Use python3 !
-python3 run.py
+set -e -x -o pipefail
+
+
+#
+# Fetch inputs (~/in/vcfgz/*)
+#
+dx-download-all-inputs --parallel
+
+# Run Variant Effect Predictor
+perl /vep/vep/vep.pl -i ./in/vcfgz/* --cache --dir /vep --offline --vcf -o ./out/annotated_vcf/out.vcf --everything --no_stats --fork `nproc`
+
+#vcf-sort < output.vcf > output.sorted.vcf
+#
+#bgzip output.sorted.vcf
+#tabix -p vcf output.sorted.vcf.gz
+#
+##
+## Upload results
+##
+#mkdir -p ~/out/vcfgz/ ~/out/tbi/
+#mv output.sorted.vcf.gz ~/out/vcfgz/"$vcfgz_prefix".annotated.vcf.gz
+#mv output.sorted.vcf.gz.tbi ~/out/tbi/"$vcfgz_prefix".annotated.vcf.gz.tbi
+dx-upload-all-outputs --parallel
